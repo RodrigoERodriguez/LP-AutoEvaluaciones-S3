827d994f3d39224bc301ad823d45ef4590a7eae9
Torito <111804493+RodrigoERodriguez@users.noreply.github.com>
2023-06-04 21:02:47 -0300

    Add files via upload

---
+++ pryRodriguezLP3_25-04.AutoEvaluacion/pryRodriguezLP_Clase25-04/frmCuadroDeDialogos.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace pryRodriguezLP_Clase25_04
+{
+    partial class frmCuadroDeDialogos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblEntradaT = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblSalidaT = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // lblEntradaT
+            // 
+            this.lblEntradaT.AutoSize = true;
+            this.lblEntradaT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEntradaT.Location = new System.Drawing.Point(22, 30);
+            this.lblEntradaT.Name = "lblEntradaT";
+            this.lblEntradaT.Size = new System.Drawing.Size(65, 16);
+            this.lblEntradaT.TabIndex = 13;
+            this.lblEntradaT.Text = "Entrada:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 73);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(94, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // lblSalidaT
+            // 
+            this.lblSalidaT.AutoSize = true;
+            this.lblSalidaT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalidaT.Location = new System.Drawing.Point(22, 73);
+            this.lblSalidaT.Name = "lblSalidaT";
+            this.lblSalidaT.Size = new System.Drawing.Size(56, 16);
+            this.lblSalidaT.TabIndex = 15;
+            this.lblSalidaT.Text = "Salida:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(125, 135);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(83, 29);
+            this.btnBuscar.TabIndex = 17;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmCuadroDeDialogos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 197);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblSalidaT);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEntradaT);
+            this.Name = "frmCuadroDeDialogos";
+            this.Text = "Romanos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblEntradaT;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblSalidaT;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ pryRodriguezLP3_25-04.AutoEvaluacion/pryRodriguezLP_Clase25-04/frmJugadores.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace pryRodriguezLP_Clase25_04
+{
+    partial class frmJugadores
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumeroT = new System.Windows.Forms.Label();
+            this.lblPiernaT = new System.Windows.Forms.Label();
+            this.lblPosicionT = new System.Windows.Forms.Label();
+            this.lblNombreT = new System.Windows.Forms.Label();
+            this.lblPesoT = new System.Windows.Forms.Label();
+            this.lblAlturaT = new System.Windows.Forms.Label();
+            this.lblAlturaV = new System.Windows.Forms.Label();
+            this.lblPosicionV = new System.Windows.Forms.Label();
+            this.lblPiernaV = new System.Windows.Forms.Label();
+            this.lblPesoV = new System.Windows.Forms.Label();
+            this.lblNombreV = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumeroT
+            // 
+            this.lblNumeroT.AutoSize = true;
+            this.lblNumeroT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroT.Location = new System.Drawing.Point(23, 23);
+            this.lblNumeroT.Name = "lblNumeroT";
+            this.lblNumeroT.Size = new System.Drawing.Size(65, 16);
+            this.lblNumeroT.TabIndex = 0;
+            this.lblNumeroT.Text = "Numero:";
+            // 
+            // lblPiernaT
+            // 
+            this.lblPiernaT.AutoSize = true;
+            this.lblPiernaT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPiernaT.Location = new System.Drawing.Point(23, 216);
+            this.lblPiernaT.Name = "lblPiernaT";
+            this.lblPiernaT.Size = new System.Drawing.Size(56, 16);
+            this.lblPiernaT.TabIndex = 1;
+            this.lblPiernaT.Text = "Pierna:";
+            // 
+            // lblPosicionT
+            // 
+            this.lblPosicionT.AutoSize = true;
+            this.lblPosicionT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPosicionT.Location = new System.Drawing.Point(23, 165);
+            this.lblPosicionT.Name = "lblPosicionT";
+            this.lblPosicionT.Size = new System.Drawing.Size(71, 16);
+            this.lblPosicionT.TabIndex = 2;
+            this.lblPosicionT.Text = "Posicion:";
+            // 
+            // lblNombreT
+            // 
+            this.lblNombreT.AutoSize = true;
+            this.lblNombreT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreT.Location = new System.Drawing.Point(23, 121);
+            this.lblNombreT.Name = "lblNombreT";
+            this.lblNombreT.Size = new System.Drawing.Size(66, 16);
+            this.lblNombreT.TabIndex = 3;
+            this.lblNombreT.Text = "Nombre:";
+            // 
+            // lblPesoT
+            // 
+            this.lblPesoT.AutoSize = true;
+            this.lblPesoT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoT.Location = new System.Drawing.Point(23, 271);
+            this.lblPesoT.Name = "lblPesoT";
+            this.lblPesoT.Size = new System.Drawing.Size(47, 16);
+            this.lblPesoT.TabIndex = 5;
+            this.lblPesoT.Text = "Peso:";
+            // 
+            // lblAlturaT
+            // 
+            this.lblAlturaT.AutoSize = true;
+            this.lblAlturaT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlturaT.Location = new System.Drawing.Point(23, 312);
+            this.lblAlturaT.Name = "lblAlturaT";
+            this.lblAlturaT.Size = new System.Drawing.Size(51, 16);
+            this.lblAlturaT.TabIndex = 4;
+            this.lblAlturaT.Text = "Altura:";
+            // 
+            // lblAlturaV
+            // 
+            this.lblAlturaV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAlturaV.Location = new System.Drawing.Point(126, 312);
+            this.lblAlturaV.Name = "lblAlturaV";
+            this.lblAlturaV.Size = new System.Drawing.Size(83, 25);
+            this.lblAlturaV.TabIndex = 11;
+            // 
+            // lblPosicionV
+            // 
+            this.lblPosicionV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPosicionV.Location = new System.Drawing.Point(126, 166);
+            this.lblPosicionV.Name = "lblPosicionV";
+            this.lblPosicionV.Size = new System.Drawing.Size(83, 25);
+            this.lblPosicionV.TabIndex = 9;
+            // 
+            // lblPiernaV
+            // 
+            this.lblPiernaV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPiernaV.Location = new System.Drawing.Point(126, 216);
+            this.lblPiernaV.Name = "lblPiernaV";
+            this.lblPiernaV.Size = new System.Drawing.Size(83, 25);
+            this.lblPiernaV.TabIndex = 8;
+            // 
+            // lblPesoV
+            // 
+            this.lblPesoV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPesoV.Location = new System.Drawing.Point(126, 271);
+            this.lblPesoV.Name = "lblPesoV";
+            this.lblPesoV.Size = new System.Drawing.Size(83, 24);
+            this.lblPesoV.TabIndex = 7;
+            // 
+            // lblNombreV
+            // 
+            this.lblNombreV.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblNombreV.Location = new System.Drawing.Point(126, 122);
+            this.lblNombreV.Name = "lblNombreV";
+            this.lblNombreV.Size = new System.Drawing.Size(83, 25);
+            this.lblNombreV.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(126, 60);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(83, 29);
+            this.btnBuscar.TabIndex = 13;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // frmJugadores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 364);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblAlturaV);
+            this.Controls.Add(this.lblPosicionV);
+            this.Controls.Add(this.lblPiernaV);
+            this.Controls.Add(this.lblPesoV);
+            this.Controls.Add(this.lblNombreV);
+            this.Controls.Add(this.lblPesoT);
+            this.Controls.Add(this.lblAlturaT);
+            this.Controls.Add(this.lblNombreT);
+            this.Controls.Add(this.lblPosicionT);
+            this.Controls.Add(this.lblPiernaT);
+            this.Controls.Add(this.lblNumeroT);
+            this.Name = "frmJugadores";
+            this.Text = "Jugadores";
+            this.Load += new System.EventHandler(this.frmJugadores_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumeroT;
+        private System.Windows.Forms.Label lblPiernaT;
+        private System.Windows.Forms.Label lblPosicionT;
+        private System.Windows.Forms.Label lblNombreT;
+        private System.Windows.Forms.Label lblPesoT;
+        private System.Windows.Forms.Label lblAlturaT;
+        private System.Windows.Forms.Label lblAlturaV;
+        private System.Windows.Forms.Label lblPosicionV;
+        private System.Windows.Forms.Label lblPiernaV;
+        private System.Windows.Forms.Label lblPesoV;
+        private System.Windows.Forms.Label lblNombreV;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
